8bc74541b1a56ae79e243f05a4da6e3ef3e7d3b2
SmokeyDays <fyq61002427@126.com>
2018-11-20 20:30:55 +0800

    注释更新。
    现在judgeConditions.js的注释会更加正确了。

--- script/judgeConditions.js
+++ script/judgeConditions.js
@@ -1,13 +1,14 @@
 var judgeConditions = {
 	/*
 	conditions: 传入一个条件，返回一个布尔值。
-		type: Function,指定特殊的判定方式。
+		type: String,指定特殊的判定方式。
 			 - not 指定一个函数，返回这个函数的相反值。
 			 - and 指定一系列的判定列表，返回这个列表的值的与和。
 			 - or 指定一系列的判定列表，返回这个列表的值的或和。
 			 
 			 - item 指定两个列表，物品名称和数量。如果物品数量均满足，返回true，否则false.
-			 - attr 提供两个列表，属性名称和数量。如果属性数量均满足，返回true，否则false。
+			 - attr 指定两个列表，属性名称和数量。如果属性数量均满足，返回true，否则false。
+			 - point 指定两个列表，点数名称和数量。如果点数数量均满足，返回true，否则false。
 		info: 提供判定的信息。
 	*/
 	judge: function(conditions){
