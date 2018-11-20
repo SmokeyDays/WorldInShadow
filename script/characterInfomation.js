61dc10000005c24b6282a1e48a8d5789092286d3
Qrison <45070153+Qrison@users.noreply.github.com>
2018-11-20 20:34:00 +0800

    漏洞修复
    修复与逻辑运算符错误所引起的逻辑判断错误

--- script/characterInfomation.js
+++ script/characterInfomation.js
@@ -1,7 +1,8 @@
 var characterInfomation = {
 	attribute: {
 		'physicalTalent': 0,
-		'directionTalent': 0
+		'directionTalent': 0,
+		'observationAbility': 0
 	},
 	points: {
 		
@@ -12,5 +13,6 @@ var characterInfomation = {
 	init: function() {
 		characterInfomation.attribute.physicalTalent = 2+8*Math.random();
 		characterInfomation.attribute.directionTalent = 2+8*Math.random();
+		characterInfomation.attribute.observationAbility = 2+8*Math.random();
 	}
 }
